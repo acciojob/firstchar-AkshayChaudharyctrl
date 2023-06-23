dc207bfd336f0b09a7184703c13967392c40ea4b
Akshay Chaudhary <124782758+AkshayChaudharyctrl@users.noreply.github.com>
2023-06-23 09:53:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-function firstChar(text) {
+function firstChar(str) {
   // your code here
+	 for (let i = 0; i < str.length; i++) {
+    if (str[i] !== ' ') {
+      return str[i];
+    }
+  }
+  return '';
 }
 
 // Do not change the code below
 
 const text = prompt("Enter text:");
 alert(firstChar(text));
+
